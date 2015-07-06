41342f8d4b9de7a6b5dfa88a81a52b4487fe70ef
Jeremy Banks <j@jeremybanks.ca>
2015-07-05 21:16:57 -0700

    m

--- loggerconfig/loggerconfig.go
+++ loggerconfig/loggerconfig.go
@@ -7,6 +7,6 @@ import "github.com/op/go-logging"
 func Use() {
 	logging.SetBackend(logging.NewBackendFormatter(
 		logging.NewLogBackend(os.Stderr, "", 0), logging.MustStringFormatter(
-			"%{color}%{module:10.10s} %{level:4.4s}%{color:reset}%{id:4.4x}%{color} %{message}\n%{color}%{module:10.10s} %{level:4.4s}%{color:reset}%{id:4.4x}%{color}     %{shortfile:-24.24s} %{longfunc:32.32s}()%{color:reset}",
+			"%{color}%{module:10.10s} %{level:4.4s}%{color:reset}%{id:4.4x}%{color} %{message}\n%{color}%{module:10.10s} %{level:4.4s}%{color:reset}%{id:4.4x}%{color}     %{shortfile:-20.20s} %{longfunc:32.32s}()%{color:reset}",
 		)))
 }
